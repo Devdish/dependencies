f255f6a56a457ae0ee3a443caa65533587672d19
disdev <64833787+Devdish@users.noreply.github.com>
2020-07-06 11:26:52 +0530

    Update Dependencies.java

--- Dependencies.java
+++ Dependencies.java
@@ -1,7 +1,43 @@
 //Dependencies List
 
+//firebase
+    implementation 'com.google.firebase:firebase-analytics:17.4.3'
+    implementation 'com.google.firebase:firebase-auth:19.3.1'
+//google Dependencies
+    implementation 'com.google.android.gms:play-services-auth:18.0.0'
+      
+      
+      
+      
+//Volley Depedencies
+      
+  implementation 'com.android.volley:volley:1.1.1'
+      
+//Picasso Dependencies
 
-
+      
+      
+      //Google Dependencies
+      API	Description in build.gradle
+Google+	:'com.google.android.gms:play-services-plus:17.0.0'
+Google :'Account Login	com.google.android.gms:play-services-auth:18.0.0'
+Google :'Actions, Base Client Library	com.google.android.gms:play-services-base:17.3.0'
+Google :'Sign In	com.google.android.gms:play-services-identity:17.0.0'
+Google :'Analytics	com.google.android.gms:play-services-analytics:17.0.0'
+Google :'Awareness	com.google.android.gms:play-services-awareness:18.0.0'
+Google :'Cast	com.google.android.gms:play-services-cast:18.1.0'
+Google :'Cloud Messaging	com.google.android.gms:play-services-gcm:17.0.0'
+Google :'Drive	com.google.android.gms:play-services-drive:17.0.0'
+Google :'Fit	com.google.android.gms:play-services-fitness:18.0.0'
+Google :'Location and Activity Recognition	com.google.android.gms:play-services-location:17.0.0'
+Google :'Mobile Ads	com.google.android.gms:play-services-ads:19.2.0'
+Mobile :'Vision	com.google.android.gms:play-services-vision:20.1.0'
+Google :'Nearby	com.google.android.gms:play-services-nearby:17.0.0'
+Google :'Panorama Viewer	com.google.android.gms:play-services-panorama:17.0.0'
+Google :'Play Game services	com.google.android.gms:play-services-games:19.0.0'
+SafetyNet	:'com.google.android.gms:play-services-safetynet:17.0.0'
+Google Pay	:'com.google.android.gms:play-services-wallet:18.0.0'
+Wear OS by Google	:'com.google.android.gms:play-services-wearable:17.0.0'
 
 
 
